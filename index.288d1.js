4a61aa73f085407ba49f2f36302141fa03e91166
dinghongfa <286540194@qq.com>
2026-05-18 22:49:05 +0800

    简单加密资源测试。

--- index.288d1.js
+++ index.288d1.js
@@ -1,4 +1,4 @@
-System.register(["./application.5f99a.js"], function (_export, _context) {
+System.register(["./application.4e575.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
