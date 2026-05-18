e1193f0f293e20a576c92022c8d29defd7052ebc
dinghongfa <286540194@qq.com>
2026-05-18 23:06:14 +0800

    简单加密资源测试。

--- index.03685.js
+++ index.03685.js
@@ -1,4 +1,4 @@
-System.register(["./application.6d61f.js"], function (_export, _context) {
+System.register(["./application.01e5b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
