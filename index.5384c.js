725cc995b53e6e99c34ebe37651f5551a71761ed
dinghongfa <286540194@qq.com>
2026-05-19 08:11:25 +0800

    初始界面用了动画。

--- index.5384c.js
+++ index.5384c.js
@@ -1,4 +1,4 @@
-System.register(["./application.01e5b.js"], function (_export, _context) {
+System.register(["./application.9e4ac.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
