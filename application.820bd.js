834fb3b155f41aca258118ca909e19771291c868
dinghongfa <286540194@qq.com>
2026-05-23 10:27:48 +0800

    更新工程。

--- application.820bd.js
+++ application.820bd.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.bd93f.json';
+          this.settingsPath = 'src/settings.09c94.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
