c0b05a2461de83410e05fe4b4d7e610c003dbea0
dinghongfa <286540194@qq.com>
2026-05-23 22:22:24 +0800

    优化包体。增加下载速度。增加小图标。

--- index.6ee4c.js
+++ index.6ee4c.js
@@ -1,4 +1,4 @@
-System.register(["./application.76153.js"], function (_export, _context) {
+System.register(["./application.497f3.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
