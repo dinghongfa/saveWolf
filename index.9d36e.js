730d2e1d8b0861f3187649401d0d6692e23488e0
dinghongfa <286540194@qq.com>
2026-05-23 12:06:03 +0800

    优化包体。增加下载速度。

--- index.9d36e.js
+++ index.9d36e.js
@@ -1,4 +1,4 @@
-System.register(["./application.fb7bc.js"], function (_export, _context) {
+System.register(["./application.ea66c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
